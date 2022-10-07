a42d29de3b26e022beff6d6cad7d99e6cae7c42a
aselimc <asc.connected@gmail.com>
2022-10-07 11:08:57 +0200

    refactor: Added initial file structure for better implementation

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+The main script to run all simulation scripts in the src/simulation folder.
+"""
+
+# We need to fill this in with the correct path to the simulation folder, after all done!
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
