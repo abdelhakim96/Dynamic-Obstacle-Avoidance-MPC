05a077652bbff625116b66bd64b359cfd25f2ae3
aselimc <asc.connected@gmail.com>
2022-10-11 17:00:46 +0200

    feat(RL): initial RL algorithms implementation
    DDPG and DQN agents are implemented, we need to have a discussion about how to align these with robot simulation

--- src/models/model_2_hakim/MPC/robot_model.py
+++ src/models/model_2_hakim/MPC/robot_model.py
@@ -2,38 +2,34 @@ from acados_template import AcadosModel
 from casadi import SX, vertcat, sin, cos, Function
 
 
-
-
 def export_parametric_robot_model() -> AcadosModel:
     model_name = 'param_robot_ode'
 
     # constants
- 
-    p = []
-    x_ref = SX.ones(5,1)
-    # set up states & controls
-    x      = SX.sym('x')
-    y      = SX.sym('y')
-    v      = SX.sym('x_d')
-    theta   = SX.sym('theta')
-    theta_d = SX.sym('theta_d')
-    
 
-    x = vertcat(x, y, v, theta,theta_d)
+    p = []
+    x_ref = SX.ones(5, 1)
+    # set up states & controls
+    x = SX.sym('x')
+    y = SX.sym('y')
+    v = SX.sym('x_d')
+    theta = SX.sym('theta')
+    theta_d = SX.sym('theta_d')
+
+    x = vertcat(x, y, v, theta, theta_d)
 
     F = SX.sym('F')
     T = SX.sym('T')
-    u = vertcat(F,T)
+    u = vertcat(F, T)
 
     # xdot
-    x_dot      = SX.sym('x_dot')
-    y_dot      = SX.sym('y_dot')
-    v_dot      = SX.sym('v_dot')
-    theta_dot   = SX.sym('theta_dot')
-    theta_ddot  = SX.sym('theta_ddot')
-   
+    x_dot = SX.sym('x_dot')
+    y_dot = SX.sym('y_dot')
+    v_dot = SX.sym('v_dot')
+    theta_dot = SX.sym('theta_dot')
+    theta_ddot = SX.sym('theta_ddot')
 
-    xdot = vertcat(x_dot , y_dot , v_dot , theta_dot,theta_ddot)
+    xdot = vertcat(x_dot, y_dot, v_dot, theta_dot, theta_ddot)
 
     # algebraic variables
     # z = None
@@ -41,9 +37,9 @@ def export_parametric_robot_model() -> AcadosModel:
     # parameters
 
     # dynamics
-    f_expl = vertcat(v * cos (theta), 
-                     v * sin (theta), 
-                     F, 
+    f_expl = vertcat(v * cos(theta),
+                     v * sin(theta),
+                     F,
                      theta_d,
                      T)
 
@@ -53,7 +49,7 @@ def export_parametric_robot_model() -> AcadosModel:
 
     model.f_impl_expr = f_impl
     model.f_expl_expr = f_expl
-    model.x = x 
+    model.x = x
     model.xdot = xdot
     model.u = u
     # model.z = z
