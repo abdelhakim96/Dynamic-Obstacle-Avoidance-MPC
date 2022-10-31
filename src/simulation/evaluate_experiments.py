e95e700e1c6074896887a154e223bcfa7d443da3
PhilippBordne <philippbordne@hotmail.com>
2022-10-31 23:02:24 +0100

    improved handling of experimental data

--- src/simulation/evaluate_experiments.py
+++ src/simulation/evaluate_experiments.py
@@ -1,16 +1,40 @@
+from fileinput import close, filename
 import numpy as np
+import json
+import os
 
-data_init = np.loadtxt('test_data_tf_20_quadratic_slack/EDGE_init_guess_random_move_lowered_slack.csv', delimiter=';')
-data_no_init = np.loadtxt('test_data_tf_20_quadratic_slack/RANDOM_init_guess_random_move_lowered_slack.csv', delimiter=';')
-data_init1 = np.loadtxt('test_data_tf_20_quadratic_slack/EDGE_init_guess_random_move.csv', delimiter=';')
-data_no_init1 = np.loadtxt('test_data_tf_20_quadratic_slack/RANDOM_init_guess_random_move.csv', delimiter=';')
-# data_no_init = np.loadtxt('test_data_tf_20/RANDOM_no_slack_adapt_init_guess.csv', delimiter=';')
-error_ratio = np.sum(data_init, 0)[0] / 100
-error_ratio_no_init = np.sum(data_no_init, 0)[0] / 100
-error_ratio1 = np.sum(data_init1, 0)[0] / 100
-error_ratio_no_init1 = np.sum(data_no_init1, 0)[0] / 100
+# data_init = np.loadtxt('test_data_tf_20_quadratic_slack/EDGE_init_guess_random_move_lowered_slack.csv', delimiter=';')
+# data_no_init = np.loadtxt('test_data_tf_20_quadratic_slack/RANDOM_init_guess_random_move_lowered_slack.csv', delimiter=';')
+# data_init1 = np.loadtxt('test_data_tf_20_quadratic_slack/EDGE_init_guess_random_move.csv', delimiter=';')
+# data_no_init1 = np.loadtxt('test_data_tf_20_quadratic_slack/RANDOM_init_guess_random_move.csv', delimiter=';')
+# # data_no_init = np.loadtxt('test_data_tf_20/RANDOM_no_slack_adapt_init_guess.csv', delimiter=';')
+# error_ratio = np.sum(data_init, 0)[0] / 100
+# error_ratio_no_init = np.sum(data_no_init, 0)[0] / 100
+# error_ratio1 = np.sum(data_init1, 0)[0] / 100
+# error_ratio_no_init1 = np.sum(data_no_init1, 0)[0] / 100
 
-print(error_ratio)
-print(error_ratio_no_init)
-print(error_ratio1)
-print(error_ratio_no_init1)
+# print(error_ratio)
+# print(error_ratio_no_init)
+# print(error_ratio1)
+# print(error_ratio_no_init1)
+
+# read out all json files in directory
+# and store them together with the np.arrays in a list of tuples
+def load_experiment_data():
+    data = []
+    for file in os.listdir('test_data'):
+        if file.endswith('.json'):
+            fpath = 'test_data/' + file
+            f = open(fpath)
+            data += [(json.load(f), np.loadtxt(fpath[:-len('_spec.json')] + '_data.csv', delimiter=';'))]
+            # print(data)
+            f.close()
+    return data
+
+def print_ratio_collision_avoidance():
+    for spec, data in load_experiment_data():
+        print(spec)
+        print(np.sum(data, axis = 0)[0] / data.shape[0])
+        
+if __name__ == '__main__':
+    print_ratio_collision_avoidance()
--- src/simulation/experiments.py
+++ src/simulation/experiments.py
@@ -2,22 +2,36 @@ import sys
 sys.path.append('../')
 
 import numpy as np
-from models.world_specification import X_MAX, X_MIN, Y_MAX, Y_MIN
+from models.world_specification import X_MAX, X_MIN, Y_MAX, Y_MIN, TF, N_OBST, N_SOLV, QP_ITER
 from robot_ocp_problem import RobotOcpProblem
+from datetime import datetime
+import json
 
 # scenarios = ['RANDOM', 'EDGE', 'CENTER']
 scenarios = ['RANDOM', 'EDGE']
 # init_guess = [True, False]
-init_guess = [True]
+init_strategy = [True]
 
-ocp = RobotOcpProblem(np.array([X_MIN + 1, Y_MIN + 1, np.pi / 4, 0, 0]), np.array([X_MAX - 1, Y_MAX - 1]), seed=0)
+slack = True
+random_move = True
+ocp = RobotOcpProblem(np.array([X_MIN + 1, Y_MIN + 1, np.pi / 4, 0, 0]), np.array([X_MAX - 1, Y_MAX - 1]))
 
 for s in scenarios:
-    for ini in init_guess:
+    for ini in init_strategy:
+        # get timestamp for file to save
+        time = datetime.now()
+        timestamp = f"{time.year:04d}{time.month:02d}{time.day:02d}_{time.hour:02d}{time.minute:02d}{time.second:02d}"
         data = np.ndarray((100, 6))
         init_guess_str = 'init_guess' if ini else 'no_init_guess'
+        # store experimental data in dictionary and write it to json file
+        experiment_dict = {"slack": slack, "random_move": random_move, "init_guess": ini, "scenario": s, "TF": TF, "N_SOLV": N_SOLV, "N_OBST": N_OBST, "QP_ITER": QP_ITER, 'interpolate_init': True}
         for i in range(100):
+            np.random.seed(i)
             print(f'{s}, {init_guess_str} solving problem: {i}')
-            ocp.set_up_new_experiment(i, s, ini, random_move=True)
+            ocp.set_up_new_experiment(s, ini, random_move=True)
             data[i] = ocp.step(400)[1:]
-        np.savetxt(f'test_data_tf_20_quadratic_slack/{s}_{init_guess_str}_random_move_lowered_slack.csv', data, delimiter=';')
+        # store the data to a .csv
+        np.savetxt(f'test_data/{timestamp}_experiment_data.csv', data, delimiter=';')
+        # store the experimental parameters to a json file
+        with open(f'test_data/{timestamp}_experiment_spec.json', 'w') as json_file:
+            json.dump(experiment_dict, json_file)
