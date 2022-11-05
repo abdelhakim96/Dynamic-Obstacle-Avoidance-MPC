42b9b23b90c5df93041c0af7d1532ab18e7bc519
aselimc <asc.connected@gmail.com>
2022-11-05 12:40:19 +0100

    feat: Deleted the uncessary code, updated the experiment files

--- src/simulation/evaluate_experiments.py
+++ src/simulation/evaluate_experiments.py
@@ -1,72 +1,126 @@
-from fileinput import close, filename
+from matplotlib.cm import ScalarMappable
 import numpy as np
 import json
 import os
 import matplotlib.pyplot as plt
 
-# data_init = np.loadtxt('test_data_tf_20_quadratic_slack/EDGE_init_guess_random_move_lowered_slack.csv', delimiter=';')
-# data_no_init = np.loadtxt('test_data_tf_20_quadratic_slack/RANDOM_init_guess_random_move_lowered_slack.csv', delimiter=';')
-# data_init1 = np.loadtxt('test_data_tf_20_quadratic_slack/EDGE_init_guess_random_move.csv', delimiter=';')
-# data_no_init1 = np.loadtxt('test_data_tf_20_quadratic_slack/RANDOM_init_guess_random_move.csv', delimiter=';')
-# # data_no_init = np.loadtxt('test_data_tf_20/RANDOM_no_slack_adapt_init_guess.csv', delimiter=';')
-# error_ratio = np.sum(data_init, 0)[0] / 100
-# error_ratio_no_init = np.sum(data_no_init, 0)[0] / 100
-# error_ratio1 = np.sum(data_init1, 0)[0] / 100
-# error_ratio_no_init1 = np.sum(data_no_init1, 0)[0] / 100
 
-# print(error_ratio)
-# print(error_ratio_no_init)
-# print(error_ratio1)
-# print(error_ratio_no_init1)
-
-# read out all json files in directory
-# and store them together with the np.arrays in a list of tuples
 def load_experiment_data():
     data = []
-    for file in os.listdir('test_data/multiple/'):
+    # Load data from json file, adjust your data path here
+    for file in os.listdir('test_data/new2/'):
         if file.endswith('.json'):
-            fpath = 'test_data/multiple/' + file
+            fpath = 'test_data/new2/' + file
             f = open(fpath)
-            data += [(json.load(f), np.loadtxt(fpath[:-len('_spec.json')] + '_data.csv', delimiter=';'))]
-            # print(data)
+            data += [(json.load(f), np.loadtxt(fpath[:-
+                      len('_spec.json')] + '_data.csv', delimiter=';'))]
             f.close()
     return data
+
 
 def print_ratio_collision_avoidance():
     for spec, data in load_experiment_data():
         print(spec)
-        print(np.sum(data, axis = 0)[0] / data.shape[0])
+        print(np.sum(data, axis=0)[0] / data.shape[0])
+
+
+def mask_data(data):
+    # Calculate the average of the ratio of reaching goal by excluding collision cases
+    temp_data = data
+    for i in range(data.shape[0]):
+        if data[i][0] != 0:
+            temp_data[i][1] = 0
+    return 100*np.sum(temp_data, axis=0)[1] / data.shape[0]
 
 
 def plot_graph():
     # Store data based on the TF and N_OBST
     data_dict = {}
     for spec, data in load_experiment_data():
-        data_dict[(spec['TF'], spec['N_OBST'])] = np.sum(data, axis = 0)[0] / data.shape[0]
-    plt.figure(1)
+        data_dict[(spec['TF'], spec['N_OBST'], spec['scenario'])
+                  ] = 100 * np.sum(data, axis=0)[0] / data.shape[0]
+    fig, ax = plt.subplots(1, 2, constrained_layout=True,
+                           sharey=True, figsize=(8, 5))
+    fig.supxlabel('Horizon')
+    fig.supylabel('Ratio of collision (%)')
     # plot data
     for key in data_dict:
-        plt.scatter(key[0], data_dict[key], c=key[1], cmap='inferno', vmin=5, vmax=30)
-    plt.grid()
-    plt.xlabel('TF')
-    plt.ylabel('Ratio of collision')
-    plt.colorbar(label='N_OBST')
-    plt.title('Collision of the robot with the obstacles with respect to horizon')
-    plt.savefig("plot.png")
+        if key[2] == "EDGE":
+            ax[0].scatter(key[0], data_dict[key], c=key[1],
+                          cmap='brg', vmin=5, vmax=30)
+        else:
+            ax[1].scatter(key[0], data_dict[key], c=key[1],
+                          cmap='brg', vmin=5, vmax=30)
+    norm = plt.Normalize(5, 30)
+    ax[0].set_axisbelow(True)
+    ax[0].grid(color='gray', linestyle='dashed')
+    ax[1].set_axisbelow(True)
+    ax[1].grid(color='gray', linestyle='dashed')
+    ax[0].set_title('EDGE')
+    ax[1].set_title('RANDOM')
+    sm = ScalarMappable(norm=norm, cmap="brg")
+    sm.set_array([])
+    cbar = fig.colorbar(sm, ax=ax[1])
+    cbar.ax.set_title('N_OBST')
+    fig.savefig("plot_collision_rate_seperate.svg")
 
     # plot goal reached average
     goal_reached_dict = {}
     for spec, data in load_experiment_data():
-        goal_reached_dict[(spec['TF'], spec['N_OBST'])] = np.sum(data, axis = 0)[1] / data.shape[0]
-    plt.figure(2)
-    for key in data_dict:
-        plt.scatter(key[0], goal_reached_dict[key], c=key[1], cmap='inferno', vmin=5, vmax=30)
-    plt.grid()
-    plt.xlabel('TF')
-    plt.ylabel('Ratio of goal reached')
-    plt.colorbar(label='N_OBST')
-    plt.title('Goal reached with respect to horizon')
-    plt.savefig("plot_goal_reached.png")
+        goal_reached_dict[(spec['TF'], spec['N_OBST'],
+                           spec['scenario'])] = mask_data(data)
+    fig, ax = plt.subplots(1, 2, constrained_layout=True,
+                           sharey=True, figsize=(8, 5))
+    fig.supxlabel('Horizon')
+    fig.supylabel('Ratio of goal reached (%)')
+    # plot data
+    for key in goal_reached_dict:
+        if key[2] == "EDGE":
+            ax[0].scatter(key[0], goal_reached_dict[key],
+                          c=key[1], cmap='brg', vmin=5, vmax=30)
+        else:
+            ax[1].scatter(key[0], goal_reached_dict[key],
+                          c=key[1], cmap='brg', vmin=5, vmax=30)
+    norm = plt.Normalize(5, 30)
+    ax[0].set_axisbelow(True)
+    ax[0].grid(color='gray', linestyle='dashed')
+    ax[1].set_axisbelow(True)
+    ax[1].grid(color='gray', linestyle='dashed')
+    ax[0].set_title('EDGE')
+    ax[1].set_title('RANDOM')
+    sm = ScalarMappable(norm=norm, cmap="brg")
+    sm.set_array([])
+    cbar = fig.colorbar(sm, ax=ax[1])
+    cbar.ax.set_title('N_OBST')
+    fig.savefig("plot_goal_reached_rate_seperate.svg")
+
+
+def plot_graph_qp_solver():
+    # Store data based on the QP_ITER
+    data_dict = {}
+    for spec, data in load_experiment_data():
+        data_dict[(spec['QP_ITER'])] = 100 * \
+            np.sum(data, axis=0)[0] / data.shape[0]
+    fig, ax = plt.subplots(2)
+    ax[0].scatter(data_dict.keys(), data_dict.values())
+    ax[0].set_ylabel('Ratio of collision (%)')
+    ax[0].set_axisbelow(True)
+    ax[0].grid(color='gray', linestyle='dashed')
+    ax[0].set_ylim([0, 20])
+    # plot goal reached average
+    goal_reached_dict = {}
+    for spec, data in load_experiment_data():
+        goal_reached_dict[(spec['QP_ITER'])] = 100 * \
+            np.sum(data, axis=0)[1] / data.shape[0]
+    ax[1].scatter(goal_reached_dict.keys(), goal_reached_dict.values())
+    ax[1].set_xlabel('QP_ITER')
+    ax[1].set_ylabel('Ratio of goal reached (%)')
+    ax[1].set_axisbelow(True)
+    ax[1].grid(color='gray', linestyle='dashed')
+    ax[1].set_ylim([80, 100])
+    plt.savefig("plot_qp_iter.svg")
+
 
 if __name__ == '__main__':
-    plot_graph()
+    plot_graph_qp_solver()
+    # plot_graph()
--- src/simulation/run_multiple_experiments.py
+++ src/simulation/run_multiple_experiments.py
@@ -1,5 +1,5 @@
 import os
-import sys
+
 
 def run_multiple_experiments():
     TFL = [0.5, 0.5, 1, 1.5, 2, 2.5, 3]
@@ -9,21 +9,37 @@ def run_multiple_experiments():
             filedata = file.read()
             filedata = filedata.replace(f'TF = {TFL[i]}', f'TF = {TFL[i+1]}')
         with open('models/world_specification.py', 'w') as file:
-                file.write(filedata)
+            file.write(filedata)
         for j in range(len(N_OBSTL)-1):
-        # Read and Overwrite the file with the new values
+            # Read and Overwrite the file with the new values
             with open('models/world_specification.py', 'r') as file:
                 filedata = file.read()
-                filedata = filedata.replace(f'N_OBST = {N_OBSTL[j]}', f'N_OBST = {N_OBSTL[j+1]}')
+                filedata = filedata.replace(
+                    f'N_OBST = {N_OBSTL[j]}', f'N_OBST = {N_OBSTL[j+1]}')
             with open('models/world_specification.py', 'w') as file:
                 file.write(filedata)
             os.system('python3 simulation/experiments.py')
 
         # Restore the N_OBST to the original values
         with open('models/world_specification.py', 'r') as file:
-                filedata = file.read()
-                filedata = filedata.replace(f'N_OBST = {N_OBSTL[-1]}', f'N_OBST = 5')
+            filedata = file.read()
+            filedata = filedata.replace(
+                f'N_OBST = {N_OBSTL[-1]}', f'N_OBST = 5')
         with open('models/world_specification.py', 'w') as file:
-                file.write(filedata)
+            file.write(filedata)
+
+
+def run_experiments_qp_solver():
+    QPS = [25, 25, 50, 100, 150]
+    for i in range(len(QPS)-1):
+        with open('models/world_specification.py', 'r') as file:
+            filedata = file.read()
+            filedata = filedata.replace(
+                f'QP_ITER = {QPS[i]}', f'QP_ITER = {QPS[i+1]}')
+        with open('models/world_specification.py', 'w') as file:
+            file.write(filedata)
+        os.system('python3 simulation/experiments.py')
+
+
 if __name__ == '__main__':
-    run_multiple_experiments()
+    run_experiments_qp_solver()
